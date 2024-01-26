7a6b5adc015d1a61ebf691a2d9b3e5d92757efed
Yanderei <78420325+Yanderei@users.noreply.github.com>
2024-01-26 11:49:31 -0400

    fixed css

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,19 +1,17 @@
-import { TextField } from "@mui/material";
+import { Button, FormControl, FormLabel, TextField } from "@mui/material";
 import React from "react";
+import "./Main.style.css";
 
 export const AddTask = () => {
   return (
     <>
       <h1>AddTask</h1>
       <div>
-        <form>
-        <TextField
-          id="status"
-          label="Status"
-          defaultValue="active"
-          helperText="Status of task"
-        />
-        </form>
+        <FormControl>
+          <FormLabel>Enter Name</FormLabel>
+          <TextField></TextField>
+          <Button>Submit</Button>
+        </FormControl>
       </div>
     </>
   );
